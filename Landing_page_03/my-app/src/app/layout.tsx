3ff6899a7c202d04c07f6dd942944980604d510a
Zija Yaseen <zijayaseen15@gmail.com>
2025-05-21 04:49:39 +0500

    update

--- Landing_page_03/my-app/src/app/layout.tsx
+++ Landing_page_03/my-app/src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import { FloatingNav } from "@/components/ui/floating-navbar";
 import { navItems } from "@/data/main";
--- Landing_page_03/my-app/src/components/Blogs.tsx
+++ Landing_page_03/my-app/src/components/Blogs.tsx
@@ -1,4 +1,3 @@
-'use client';
 import Image from 'next/image';
 import Link from 'next/link';
 import { posts } from '../data/main';
